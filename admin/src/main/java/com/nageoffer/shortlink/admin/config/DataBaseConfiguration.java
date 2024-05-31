9322a9e00e74fb19b5efb7fffdfd249ad37266c5
DMS <1605165925@qq.com>
2024-05-31 15:59:29 +0800

    feature: 提交Mysql数据库分页插件

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,31 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName:DataBaseConfig
+ * Description:
+ * 数据库持久层配置类
+ * @Author DubPAN
+ * @Create2024/5/31 15:56
+ * @Version 1.0
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
