e8751325ff30be192afc4901c56f1cdf6a2861ae
DMS <1605165925@qq.com>
2024-06-05 15:21:54 +0800

    feture:获取短链接对应原网址的网站标题和图标

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UrlTitleController.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.admin.controller;
+
+
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.ShortLinkRemoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName:UrlTitleController
+ * Description:
+ * URL标题控制层
+ * @Author DubPAN
+ * @Create2024/6/4 20:48
+ * @Version 1.0
+ */
+@RestController
+@RequiredArgsConstructor
+public class UrlTitleController {
+
+    //TODO 后续重构为SpringCloud Feign 调用
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 根据 URL 获取对应网站的标题
+     */
+    @GetMapping("/api/short-link/admin/v1/title")
+    public Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        return shortLinkRemoteService.getTitleByUrl(url);
+    }
+}
