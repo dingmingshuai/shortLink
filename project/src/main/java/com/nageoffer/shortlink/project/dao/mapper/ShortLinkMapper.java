25600e82fbc530a74828d9b78503f00a888eeac8
DMS <1605165925@qq.com>
2024-06-15 16:43:23 +0800

    feature:开发短链接之汇总统计

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -2,6 +2,8 @@ package com.nageoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * ClassName:ShortLinkMapper
@@ -12,4 +14,15 @@ import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
  * @Version 1.0
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
