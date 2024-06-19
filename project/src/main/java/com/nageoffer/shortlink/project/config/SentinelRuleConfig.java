bf74d03e5932a821a79b61c22fabda6186ef97e7
DMS <1605165925@qq.com>
2024-06-19 15:24:15 +0800

    feature:短链接中台创建短链接接口请求限制

---
+++ project/src/main/java/com/nageoffer/shortlink/project/config/SentinelRuleConfig.java
@@ -0,0 +1,33 @@
+package com.nageoffer.shortlink.project.config;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName:SentinelRuleConfig
+ * Description:
+ * 初始化限流配置
+ * @Author DubPAN
+ * @Create2024/6/19 15:19
+ * @Version 1.0
+ */
+@Component
+public class SentinelRuleConfig implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule createOrderRule = new FlowRule();
+        createOrderRule.setResource("create_short-link");
+        createOrderRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        createOrderRule.setCount(1);
+        rules.add(createOrderRule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
---
+++ project/src/main/java/com/nageoffer/shortlink/project/handler/CustomBlockHandler.java
@@ -0,0 +1,21 @@
+package com.nageoffer.shortlink.project.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.nageoffer.shortlink.project.common.convention.result.Result;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+/**
+ * ClassName:CustomBlockHandler
+ * Description:
+ *  自定义流控策略
+ * @Author DubPAN
+ * @Create2024/6/19 15:22
+ * @Version 1.0
+ */
+public class CustomBlockHandler {
+    //自定义流控策略  简单回应访问人数过多，要求稍后发访问
+    public static Result<ShortLinkCreateRespDTO> createShortLinkBlockHandlerMethod(ShortLinkCreateReqDTO requestParam, BlockException exception) {
+        return new Result<ShortLinkCreateRespDTO>().setCode("B100000").setMessage("当前访问网站人数过多，请稍后再试...");
+    }
+}
